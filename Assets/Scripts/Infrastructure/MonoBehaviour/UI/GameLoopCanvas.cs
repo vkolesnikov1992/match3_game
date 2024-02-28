d1ea1429213532deb3c919879ce142e1ede256e8
Vladimir Kolesnikov <mrgoodwin1992@gmail.com>
2024-02-28 17:06:44 +0600

    added UI

---
+++ Assets/Scripts/Infrastructure/MonoBehaviour/UI/GameLoopCanvas.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using UnityEngine;
+using UnityEngine.UI;
+using Zenject;
+
+namespace Infrastructure.MonoBehaviour.UI
+{
+    public class GameLoopCanvas : UnityEngine.MonoBehaviour
+    {
+        [SerializeField] private Button _nextLevelButton;
+        [SerializeField] private Button _replayButton;
+        
+        [Inject]
+        public void Construct()
+        {
+            throw new NotImplementedException();
+        }
+
+        private void Awake()
+        {
+            _nextLevelButton.onClick.AddListener(NextLevel);
+            _replayButton.onClick.AddListener(ReplayLevel);
+        }
+
+        private void NextLevel()
+        {
+            throw new NotImplementedException();
+        }
+
+        private void ReplayLevel()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
