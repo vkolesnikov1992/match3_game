544ed54cf31e721bfb5a1f133c97f58d1413389f
Vladimir Kolesnikov <mrgoodwin1992@gmail.com>
2024-02-28 15:34:43 +0600

    added input service

---
+++ Assets/Scripts/Infrastructure/MonoBehaviour/InputService/InputService.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using Infrastructure.Services.InputService.Interfaces;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+namespace Infrastructure.MonoBehaviour.InputService
+{
+    public class InputService : UnityEngine.MonoBehaviour, IInputService, IPointerDownHandler, IPointerUpHandler
+    {
+        public event Action<Vector2Int, Vector2Int> OnSwipeDetected;
+        
+        public void OnPointerDown(PointerEventData eventData)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void OnPointerUp(PointerEventData eventData)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ Assets/Scripts/Infrastructure/Services/InputService/Interfaces/IInputService.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using UnityEngine;
+
+namespace Infrastructure.Services.InputService.Interfaces
+{
+    public interface IInputService
+    {
+        event Action<Vector2Int, Vector2Int> OnSwipeDetected;
+    }
+}
