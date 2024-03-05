718b3e124c43b40fc7c4c44d4da4f34a1a69e977
Vladimir Kolesnikov <mrgoodwin1992@gmail.com>
2024-03-05 18:41:09 +0500

    added game bootstrapper

---
+++ Assets/Scripts/Infrastructure/MonoBehaviour/Bootstrap/Bootstrapper.cs
@@ -0,0 +1,22 @@
+﻿using Infrastructure.StateMachines.GameStateMachine;
+using Infrastructure.StateMachines.GameStateMachine.States;
+using Zenject;
+
+namespace Infrastructure.MonoBehaviour.Bootstrap
+{
+    public class Bootstrapper : UnityEngine.MonoBehaviour
+    {
+        private DiContainer _diContainer;
+        
+        [Inject]
+        public void Construct(DiContainer diContainer)
+        {
+            _diContainer = diContainer;
+        }
+        private void Awake()
+        {
+            GameStateMachine gameStateMachine = new GameStateMachine(_diContainer);
+            gameStateMachine.Enter<BootstrapState>();
+        }
+    }
+}
