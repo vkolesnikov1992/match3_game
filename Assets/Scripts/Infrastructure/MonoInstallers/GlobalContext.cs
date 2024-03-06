f74977dbc0427d840492c23037a60fd539c45997
Vladimir Kolesnikov <mrgoodwin1992@gmail.com>
2024-03-06 18:17:41 +0500

    MonoInstallers updated

--- Assets/Scripts/Infrastructure/MonoInstallers/GlobalContext.cs
+++ Assets/Scripts/Infrastructure/MonoInstallers/GlobalContext.cs
@@ -1,4 +1,10 @@
-﻿using Zenject;
+﻿using Infrastructure.Factory.GameFactory;
+using Infrastructure.Factory.GameFactory.Interfaces;
+using Infrastructure.Services.InputService;
+using Infrastructure.Services.InputService.Interfaces;
+using Infrastructure.Services.Json;
+using Infrastructure.Services.Json.Interfaces;
+using Zenject;
 
 namespace Infrastructure.MonoInstallers
 {
@@ -6,7 +12,21 @@ namespace Infrastructure.MonoInstallers
     {
         public override void InstallBindings()
         {
-            
+            RegisterServices();
+            BindInstaller();
+        }
+        
+        private void RegisterServices()
+        {
+            Container.Bind<IGameFactory>().To<GameFactory>().AsSingle();
+            Container.Bind<ISerializer>().To<NewtonsoftSerializer>().AsSingle();
+
+            Container.Bind<IInputService>().To<InputService>().AsSingle();
+        }
+
+        private void BindInstaller()
+        {
+            Container.BindInterfacesTo<GlobalContext>().FromInstance(this).AsSingle();
         }
     }
 }
--- Assets/Scripts/Infrastructure/MonoInstallers/SceneContexts/GameLoopContext.cs
+++ Assets/Scripts/Infrastructure/MonoInstallers/SceneContexts/GameLoopContext.cs
@@ -1,10 +1,23 @@
-﻿using Zenject;
+﻿using Infrastructure.MonoBehaviour.InputService;
+using Infrastructure.Services.InputService.Interfaces;
+using UnityEngine;
+using Zenject;
 
 namespace Infrastructure.MonoInstallers.SceneContexts
 {
     public class GameLoopContext : MonoInstaller
     {
         public override void InstallBindings()
+        {
+            RegisterServices();
+            BindInstaller();
+        }
+        private void RegisterServices()
+        {
+            
+        }
+
+        private void BindInstaller()
         {
             
         }
