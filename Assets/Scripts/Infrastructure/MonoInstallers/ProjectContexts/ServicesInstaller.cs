26fce6f1bedeeb9529a50b50f874d712f8ddbfea
Vladimir Kolesnikov <mrgoodwin1992@gmail.com>
2024-03-07 18:32:25 +0500

    updated installers

---
+++ Assets/Scripts/Infrastructure/MonoInstallers/ProjectContexts/ServicesInstaller.cs
@@ -0,0 +1,28 @@
+﻿using Infrastructure.Factory.GameFactory;
+using Infrastructure.Services.DataProvider;
+using Infrastructure.Services.InputService;
+using Infrastructure.Services.Json;
+using Infrastructure.Services.SaveLoadService;
+using Zenject;
+
+namespace Infrastructure.MonoInstallers.ProjectContexts
+{
+    public class ServicesInstaller : MonoInstaller
+    {
+        public override void InstallBindings()
+        {
+            RegisterServices();
+        }
+        
+        private void RegisterServices()
+        {
+            Container.BindInterfacesTo<GameFactory>().AsSingle();
+            Container.BindInterfacesTo<NewtonsoftSerializer>().AsSingle();
+
+            Container.BindInterfacesTo<InputService>().AsSingle();
+            Container.BindInterfacesAndSelfTo<DataProvider>().AsSingle();
+            Container.BindInterfacesTo<PlayerPrefsService>().AsSingle();
+            Container.BindInterfacesTo<ServicesInstaller>().FromInstance(this).AsSingle();
+        }
+    }
+}
