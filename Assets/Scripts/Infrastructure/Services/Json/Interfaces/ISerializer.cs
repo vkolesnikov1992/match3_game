0cd8203c221a1810fdcfc220041d64421961f495
Vladimir Kolesnikov <mrgoodwin1992@gmail.com>
2024-03-07 18:30:06 +0500

    updated serializer

--- Assets/Scripts/Infrastructure/Services/Json/Interfaces/ISerializer.cs
+++ Assets/Scripts/Infrastructure/Services/Json/Interfaces/ISerializer.cs
@@ -5,7 +5,9 @@ namespace Infrastructure.Services.Json.Interfaces
     public interface ISerializer
     {
         string Serialize(object obj);
+
+        T Deserialize<T>(string json);
         
-        UniTask<T> Deserialize<T>(string id, bool isSingleObject);
+        UniTask<T> DeserializeConfig<T>(string id, bool isSingleObject);
     }
 }
--- Assets/Scripts/Infrastructure/Services/Json/NewtonsoftSerializer.cs
+++ Assets/Scripts/Infrastructure/Services/Json/NewtonsoftSerializer.cs
@@ -1,20 +1,21 @@
-﻿using System.Threading.Tasks;
-using Cysharp.Threading.Tasks;
+﻿using Cysharp.Threading.Tasks;
 using Infrastructure.Services.Json.Interfaces;
 using Newtonsoft.Json;
 using Newtonsoft.Json.Linq;
 using UnityEngine;
 
+
 namespace Infrastructure.Services.Json
 {
     public class NewtonsoftSerializer : ISerializer
     {
-        public string Serialize(object obj)
-        {
-            return JsonConvert.SerializeObject(obj);
-        }
+        public string Serialize(object obj) => 
+            JsonConvert.SerializeObject(obj);
 
-        public async UniTask<T> Deserialize<T>(string id, bool isSingleObject)
+        public T Deserialize<T>(string json) => 
+            JsonConvert.DeserializeObject<T>(json);
+
+        public async UniTask<T> DeserializeConfig<T>(string id, bool isSingleObject)
         {
             TextAsset textAsset = Resources.Load<TextAsset>(id);
             string data = textAsset.ToString();
