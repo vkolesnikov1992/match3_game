9a876b60f459e7a6cc7000a2b76a847ad385928f
Vladimir Kolesnikov <mrgoodwin1992@gmail.com>
2024-03-11 14:40:46 +0500

    implement CreateBall method

--- Assets/Scripts/Infrastructure/Factory/GameFactory/GameFactory.cs
+++ Assets/Scripts/Infrastructure/Factory/GameFactory/GameFactory.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using Infrastructure.Factory.GameFactory.Interfaces;
 using Infrastructure.MonoBehaviour.View.Core;
+using Infrastructure.Services.DataProvider.Interfaces;
 using UnityEngine;
 using UnityEngine.U2D;
 using Object = UnityEngine.Object;
@@ -11,14 +12,25 @@ namespace Infrastructure.Factory.GameFactory
 {
     public class GameFactory : IGameFactory
     {
+        private readonly IConfigDataProvider _configDataProvider;
         private readonly Dictionary<string, Sprite[]> _animationContainer = new();
-        private Transform _cubeViewHolder;
-        private CubeView _cubeViewPrefab;
 
+        private Sprite[] _ballsSprites;
+            
+        private Transform _cubeViewHolder;
+        private Transform _ballViewHolder;
+        
+        private CubeView _cubeViewPrefab;
+        private BallView _ballViewPrefab;
+
+        public GameFactory(IConfigDataProvider configDataProvider)
+        {
+            _configDataProvider = configDataProvider;
+        }
 
         public CubeView CreateCube(int x, int y, int type)
         {
-            string[] atlases = { $"{type}_idle", $"{type}_destroy" };
+            string[] atlases = { $"Atlases/{type}_idle", $"Atlases/{type}_destroy" };
 
             LoadAtlases(atlases);
 
@@ -34,14 +46,38 @@ namespace Infrastructure.Factory.GameFactory
 
             Vector3 position = new Vector3(x, y, 0);
             CubeView cubeView = Object.Instantiate(_cubeViewPrefab, position, Quaternion.identity, _cubeViewHolder);
-            cubeView.Initialize(_animationContainer[atlases[0]], _animationContainer[atlases[1]]);
+            
+            int animationSpeed = _configDataProvider.ConfigsDataContainer.GameSettings.CubeAnimationSpeed;
+            
+            cubeView.Initialize(_animationContainer[atlases[0]], _animationContainer[atlases[1]], animationSpeed);
             
             return cubeView;
         }
 
-        public void CreateBall(int type)
+        public BallView CreateBall(Vector3 position)
         {
+            if (_ballViewPrefab == null)
+            {
+                _ballViewPrefab = LoadPrefab<BallView>();
+            }
             
+            if (_ballViewHolder == null)
+            {
+                _ballViewHolder = new GameObject("BallHolder").transform;
+            }
+
+            if (_ballsSprites == null)
+            {
+                SpriteAtlas atlas = Resources.Load<SpriteAtlas>($"Atlases/balloons");
+                Sprite[] sprites = new Sprite[atlas.spriteCount];
+                atlas.GetSprites(sprites);
+                _ballsSprites = sprites;
+            }
+            
+            BallView ballView = Object.Instantiate(_ballViewPrefab, position, Quaternion.identity, _ballViewHolder);
+            ballView.SetSprites(_ballsSprites);
+            
+            return ballView;
         }
 
         private bool IsAtlasExist(Dictionary<string, Sprite[]> container, string path)
@@ -92,14 +128,13 @@ namespace Infrastructure.Factory.GameFactory
             return prefab;
         }
         
-        private void LoadAtlases(params string[] atlasNames)
+        private void LoadAtlases(string[] atlasNames)
         {
             foreach (string atlasName in atlasNames)
             {
-                string atlasPath = $"Atlases/{atlasName}";
-                if (!IsAtlasExist(_animationContainer, atlasPath))
+                if (!IsAtlasExist(_animationContainer, atlasName))
                 {
-                    LoadAtlas(_animationContainer, atlasPath);
+                    LoadAtlas(_animationContainer, atlasName);
                 }
             }
         } 
--- Assets/Scripts/Infrastructure/Factory/GameFactory/Interfaces/IGameFactory.cs
+++ Assets/Scripts/Infrastructure/Factory/GameFactory/Interfaces/IGameFactory.cs
@@ -1,10 +1,12 @@
 ﻿using Infrastructure.MonoBehaviour.View.Core;
+using UnityEngine;
 
 namespace Infrastructure.Factory.GameFactory.Interfaces
 {
     public interface IGameFactory
     {
         CubeView CreateCube(int x, int y, int type);
-        
+
+        BallView CreateBall(Vector3 position);
     }
 }
