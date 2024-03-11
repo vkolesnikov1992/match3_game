7b569afe8a25f9471bbc2c6836d9bcbbbafad6b3
Vladimir Kolesnikov <mrgoodwin1992@gmail.com>
2024-03-11 12:33:02 +0500

    added mono provider

---
+++ Assets/Scripts/Infrastructure/MonoBehaviour/MonoProvider/MonoProvider.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using Infrastructure.Services.MonoProvider.Interfaces;
+
+namespace Infrastructure.MonoBehaviour.MonoProvider
+{
+    public class MonoProvider : UnityEngine.MonoBehaviour, IMonoProvider
+    {
+        public event Action<bool> OnApplicationPauseEvent;
+        private void OnApplicationPause(bool pause)
+        {
+            OnApplicationPauseEvent?.Invoke(pause);
+        }
+    }
+}
---
+++ Assets/Scripts/Infrastructure/Services/MonoProvider/Interfaces/IMonoProvider.cs
@@ -0,0 +1,9 @@
+﻿using System;
+
+namespace Infrastructure.Services.MonoProvider.Interfaces
+{
+    public interface IMonoProvider
+    {
+        event Action<bool> OnApplicationPauseEvent;
+    }
+}
