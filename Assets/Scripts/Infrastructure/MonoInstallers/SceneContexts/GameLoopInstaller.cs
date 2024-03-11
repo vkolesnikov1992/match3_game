5a94180b41a5cac4dc51325f959ad2595e52da89
Vladimir Kolesnikov <mrgoodwin1992@gmail.com>
2024-03-11 14:41:30 +0500

    updated game loop installer

--- Assets/Scripts/Infrastructure/MonoInstallers/SceneContexts/GameLoopInstaller.cs
+++ Assets/Scripts/Infrastructure/MonoInstallers/SceneContexts/GameLoopInstaller.cs
@@ -1,4 +1,6 @@
-﻿using Infrastructure.Services.InputService.Interfaces;
+﻿using Infrastructure.Services.CameraService;
+using Infrastructure.Services.InputService.Interfaces;
+using Infrastructure.Systems.Core;
 using Zenject;
 
 namespace Infrastructure.MonoInstallers.SceneContexts
@@ -8,11 +10,20 @@ namespace Infrastructure.MonoInstallers.SceneContexts
         public override void InstallBindings()
         {
             RegisterInput();
+            RegisterGameSystems();
         }
 
         private void RegisterInput()
         {
             Container.Bind<IInputService>().FromComponentInHierarchy().AsSingle();
+            Container.BindInterfacesTo<CameraService>().AsSingle();
+        }
+
+        private void RegisterGameSystems()
+        {
+            Container.BindInterfacesTo<GameplaySystem>().AsSingle();
+            Container.BindInterfacesAndSelfTo<Match3System>().AsSingle();
+            Container.BindInterfacesTo<BallSystem>().AsSingle();
         }
     }
 }
